a6210789d21dd34d706954f9227ff6c2c9ce5bff
pzc <nishishabi10143371@qq.com>
2021-05-17 23:37:49 +0800

    YouTobe第三次提交

--- lib/home/item/view.dart
+++ lib/home/item/view.dart
@@ -8,21 +8,37 @@ Widget buildView(ItemState state, Dispatch dispatch, ViewService viewService) {
     body: new Column(
       children: <Widget>[
         new Row(children: <Widget>[
-          new Image.network(
-            'https://raw.githubusercontent.com/think-ing/flutter_demo/master/images/ba.jpg',
-            color: Colors.blue,
-            colorBlendMode: BlendMode.colorDodge,
-          )
+          // new Image.network(
+          //   'https://raw.githubusercontent.com/think-ing/flutter_demo/master/images/ba.jpg',
+          //   width: 6.0,
+          //   height: 6.0,
+          //   color: Colors.blue,
+          //   colorBlendMode: BlendMode.colorDodge,
+          // )
         ]),
         new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            new Icon(Icons.person),
-            new Column(
-              children: <Widget>[
-                new Text(state.homeData.title),
-                new Text(state.homeData.content),
-                new Text(state.homeData.time),
-              ],
+            new Container(
+              child: new Icon(Icons.person),
+              padding: new EdgeInsets.fromLTRB(10, 0, 10, 0),
+            ),
+            new Container(
+              child: new Container(
+                color: Colors.black45,
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    new Text(state.homeData.title),
+                    new Text(state.homeData.content),
+                    new Text(state.homeData.time),
+                  ],
+                ),
+              ),
+              padding: new EdgeInsets.fromLTRB(0, 10, 10, 0),
+              margin: new EdgeInsets.fromLTRB(0, 0, 0, 0),
+              width:  MediaQuery.of(viewService.context).size.width-50,
             )
           ],
         ),
